7028b9ce9a83c6bc20e05a66e753ee2e119934fd
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-05-22 14:49:46 -0300

    Repetição otimizada com For

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ function tocaSom(idElementAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while(contador < listaDeTeclas.length) {
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
     
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
@@ -15,5 +13,4 @@ while(contador < listaDeTeclas.length) {
     listaDeTeclas[contador].onclick = function () {
         tocaSom(idAudio);
     };
-    contador = contador + 1;
 }
