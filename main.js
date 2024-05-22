5cfdf3b8c2ea477a0e921327d8eb45db0ba85455
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-05-22 14:39:42 -0300

    Textos dinâmicos

--- main.js
+++ main.js
@@ -7,9 +7,13 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while(contador < listaDeTeclas.length) {
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
     listaDeTeclas[contador].onclick = function () {
-        tocaSom('#som_tecla_pom');
+        tocaSom(idAudio);
     };
     contador = contador + 1;
-    console.log(contador);
 }
