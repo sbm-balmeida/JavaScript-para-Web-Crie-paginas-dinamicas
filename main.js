bdb43dca122e3246623793816e84d0d03a40f18b
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-05-23 10:00:29 -0300

    Condições no código

--- main.js
+++ main.js
@@ -10,12 +10,14 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++) {
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`; //template string
 
-    listaDeTeclas[contador].onclick = function () {
+    tecla.onclick = function() {
         tocaSom(idAudio);
-    };
+    }
 
-    tecla.onkeydown = function() {
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function(evento) {
+        if (evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
     }
 
     tecla.onkeyup = function() {
